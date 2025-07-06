0cc0086bd97f8b0ccfd020a024163eea8dc9a18e
sachinchib <sachinrajput0133@gmail.com>
2025-07-06 10:55:17 +0530

    Deploy to GitHub Pages

---
+++ _next/static/chunks/pages/404-c98cf5ac37396029.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{394:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return _(1574)}])},1574:function(n,u,_){"use strict";_.r(u);var t=_(5893);_(7294),u.default=()=>(0,t.jsx)(t.Fragment,{})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=394)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/500-ff2ad281b3e225c4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[573],{5954:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/500",function(){return _(8647)}])},8647:function(n,u,_){"use strict";_.r(u);var i=_(5893);_(7294),u.default=()=>(0,i.jsx)("div",{children:"500"})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=5954)}),_N_E=n.O()}]);
